8971e49d52c23251d4a0ea19d934752738a61baa
Mário César Silva <mcgato33@hotmail.com>
2020-08-30 19:22:08 -0100

    refaturar codigo

--- script.js
+++ script.js
@@ -1,51 +1,64 @@
-/**
- * converter o "favorites" em um array usando o JSON.parse() porque o localStorage é string
- * tenho que guarda-lo como array tambem (pegar num array e guarda-lo como array e não como string)
- * usando o JSON.strigify() -> vai ser uma etrutura guardado estilo array.
- */
-
+// variaveis
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-
-// pegar image externa
-async function getExternalImage() {
-  const response = await fetch('https://source.unsplash.com/random');
-
-  document.querySelector('.image').innerHTML = `<img src="${response.url}" />`;
-};
-
-getExternalImage();
-
+const imageContainer = document.querySelector('.image');
 
 // clicar no botão, pegar imagem externa
-document.querySelector('button').onclick = () => {
-    getExternalImage();
-};
+document.querySelector('button').onclick = () => updateImage();
 
 
 // clicar em cima da image
-document.querySelector('.image').onclick = () => {
-  // pegar a imagem de estrela
-  const imageContainer = document.querySelector('.image');
+imageContainer.onclick = () => updateAll();
 
-  // pegar url da imagem
+//      ---- Metodos -----
+
+function getState() {
   const imageSource = document.querySelector('.image img').src;
-  console.log(imageSource);
 
-
-  // remover -> verificar se a imagem esta na localStorage se estiver remova
   const index = favorites.indexOf(imageSource);
   const existsInLocalStorage = index != -1;
 
+  return { imageSource, index, existsInLocalStorage };
+}
+
+function updateAll() {
+  updateFavorites();
+  updateClasses();
+}
+
+function updateFavorites() {
+  const { existsInLocalStorage, index, imageSource } = getState();
+
   if(existsInLocalStorage) {
     favorites.splice(index, 1);
-    imageContainer.classList.remove('fav');
+
   } else {
     // introduzir as url das imagens dentro do array
     favorites.push(imageSource);
-    imageContainer.classList.add('fav');
   }
 
   // salvar o array de imagens no localStorage
   localStorage.setItem('favorites', JSON.stringify(favorites));
 }
 
+function updateClasses() {
+  
+
+  if(existsInLocalStorage) {
+    imageContainer.classList.remove('fav');
+  } else {
+    imageContainer.classList.add('fav');
+  }
+}
+
+async function getExternalImage() {
+  const response = await fetch('https://source.unsplash.com/random');
+
+  imageContainer.innerHTML = `<img src="${response.url}" />`;
+};
+
+getExternalImage();
+
+function updateImage() {
+  getExternalImage();
+  updateClasses();
+}
