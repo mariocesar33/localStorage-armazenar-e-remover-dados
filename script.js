5bf2c1d2dcc1b68d0a808a5f22b6702fbb968b17
Mário César Silva <mcgato33@hotmail.com>
2020-09-03 02:04:58 -0100

    segunda fase

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // variaveis
 let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
 const imageContainer = document.querySelector('.image');
+const imageList = document.querySelector('.favorios');
+
 
 // eventos
 document.querySelector('button').onclick = () => updateImage();
@@ -10,6 +12,7 @@ imageContainer.onclick = () => updateAll();
 
 function getState() {
   const imageSource = document.querySelector('.image img').src;
+  console.log(imageSource);
 
   const index = favorites.indexOf(imageSource);
   const existsInLocalStorage = index != -1;
@@ -38,6 +41,8 @@ function updateClasses() {
   if(existsInLocalStorage) {
     imageContainer.classList.add('fav');
   } 
+
+  Refresh();
 }
 
 async function updateImage() {
@@ -51,6 +56,31 @@ async function getExternalImage() {
   imageContainer.innerHTML = `<img src="${response.url}" />`;
 };
 
+function renderImage(item, index) {
+  imageList.innerHTML += `<img class="demo cursor" src="${item}" style="width:100%" onclick="myFunction(this)" alt="The Woods"/>`;
+}
+
+// percorrer Array de favoritos
+function listFavorites() {
+  let listFavorites = JSON.parse(localStorage.getItem('favorites'));
+
+  listFavorites.forEach(renderImage);
+
+  console.log(listFavorites);
+}
+
+function myFunction(imgs) {
+  imageContainer.innerHTML = `<img src="${imgs.src}" />`;
+}
+
+function Refresh() {
+  window.parent.location = window.parent.location.href;
+}
+
 getExternalImage();
+
+listFavorites();
+
+
 
 
